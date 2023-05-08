bf73f97f92c824789fe6ccd0792f6c01c1403a85
Phil Dangerfield <philipdangerfield@Philips-MacBook-Pro.local>
2023-05-08 16:25:00 -0600

    added routes for notes and index.js

---
+++ Develop/routes/notes.js
@@ -0,0 +1,31 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+    console.log(`${req.method} request received to get notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🚀`);
+    }
+    else {
+        res.error('Error in adding note');
+    }
+});
+
+module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
 const fs = require('fs');
-const api = require('./')
+const api = require('./routes/index.js')
 
 
 const app = express();
